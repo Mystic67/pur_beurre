526df4fcffc8d64c764e643a43420f120589dadd
Mystic67 <webmaster@passion-geek.com>
2019-12-02 19:09:07 +0100

    Add DB settings comments

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',  # on utilise l'adaptateur postgresql
         'NAME': 'pur_beurre',  # le nom de notre base de donnees creee precedemment
-        'USER': 'sebastien',  # attention : remplacez par votre nom d'utilisateur
-        'PASSWORD': '',
-        'HOST': '',
+        'USER': 'sebastien',  # Remplacez par le nom d'utilisateur utilisé lors de l'installation du serveur
+        'PASSWORD': '', # Entrer le mot de passe de configuration de votre serveur
+        'HOST': '', # Entrer l'addresse de votre serveur s'il n'est pas en local
         'PORT': '5432',
     }
 }
